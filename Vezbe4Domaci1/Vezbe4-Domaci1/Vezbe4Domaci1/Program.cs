3386e763cb282628d367668cdd9a8a6f19ae567c
nidzone <nidzone021@gmail.com>
2016-10-31 18:43:30 +0100

    Upload zadatka

---
+++ Vezbe4Domaci1/Vezbe4-Domaci1/Vezbe4Domaci1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vezbe4Domaci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//gitTest
+            int[] koloneZamena = new int[2];
+
+            Console.WriteLine("Unesite broj redova matrice: ");
+            int redovi = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite broj kolona matrice: ");
+            int kolone = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite elemente matrice: ");
+            int[,] pocetnaMatrica = new int[kolone, redovi];
+            for (int i = 0; i < pocetnaMatrica.GetLength(0); i++)
+            {
+                for (int j = 0; j < pocetnaMatrica.GetLength(1); j++) pocetnaMatrica[i, j] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Uneta matrica: ");
+            for (int i = 0; i < pocetnaMatrica.GetLength(0); i++)
+            {
+                for (int j = 0; j < pocetnaMatrica.GetLength(1); j++) Console.Write(pocetnaMatrica[i, j]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Unesite brojeve kolona koje zelite da zamenite: ");
+            for (int i = 0; i < 2; i++) koloneZamena[i] = Int32.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine("Rezultirajuca matrica: ");
+            int[] tempNiz = new int[kolone];
+            for (int z = 0; z < pocetnaMatrica.GetLength(0); z++)
+            {
+                tempNiz[z] = pocetnaMatrica[z, koloneZamena[0]];
+                pocetnaMatrica[z, koloneZamena[0]] = pocetnaMatrica[z, koloneZamena[1]];
+                pocetnaMatrica[z, koloneZamena[1]] = tempNiz[z];
+            }
+
+            for (int i = 0; i < pocetnaMatrica.GetLength(0); i++)
+            {
+                for (int j = 0; j < pocetnaMatrica.GetLength(1); j++) Console.Write(pocetnaMatrica[i, j]);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vezbe4Domaci1/Vezbe4-Domaci1/Vezbe4Domaci1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vezbe4Domaci1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vezbe4Domaci1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29494da9-e0f7-474b-bb9f-f7d9dba86956")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
